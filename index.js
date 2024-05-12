a0326d4a245d23df7f3cdb25420e988a23ac63a2
sfasfasf2fs <gs1jcoyne35@hotmail.com>
2024-05-12 21:31:35 +0700

    feat

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-function moveZeroes(nums) {
-  let index = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      nums[index++] = nums[i];
+function calculate(s) {
+  const stack = [];
+  let num = 0;
+  let sign = "+";
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (!isNaN(parseInt(char)) && char !== " ") {
+      num = num * 10 + parseInt(char);
+    }
+    if (isNaN(parseInt(char)) || i === s.length - 1) {
+      if (sign === "+") stack.push(num);
+      else if (sign === "-") stack.push(-num);
+      else if (sign === "*") stack.push(stack.pop() * num);
+      else if (sign === "/") stack.push(parseInt(stack.pop() / num));
+      num = 0;
+      sign = char;
     }
   }
-  for (let i = index; i < nums.length; i++) {
-    nums[i] = 0;
-  }
-  return nums;
+  return stack.reduce((acc, val) => acc + val, 0);
 }
